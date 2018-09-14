3d1140c2d54eefba47e400408b29afa3440b5f37
Roberto De Almeida <roberto@dealmeida.net>
2018-09-14 08:43:53 -0700

    Move translator out of unit test

--- tests/context.py
+++ tests/context.py
@@ -4,3 +4,4 @@ sys.path.insert(0, os.path.abspath(
     os.path.join(os.path.dirname(__file__), '..')))
 
 import gsheetsdb
+from gsheetsdb.translator import translate
--- tests/test_translation.py
+++ tests/test_translation.py
@@ -1,42 +1,10 @@
 # -*- coding: utf-8 -*-
 
-from .context import gsheetsdb
+from .context import gsheetsdb, translate
 
 import unittest
 
 from moz_sql_parser import parse, format
-
-
-def find_value_clauses(json):
-    if isinstance(json, dict):
-        if 'value' in json:
-            yield json
-        else:
-            for value in json.values():
-                yield from find_value_clauses(value)
-    elif isinstance(json, list):
-        for element in json:
-            yield from find_value_clauses(element)
-
-
-def replace_value(json, replacement_map):
-    for key, value in json.items():
-        if isinstance(value, dict):
-            replace_value(value, replacement_map)
-        elif value in replacement_map:
-            json[key] = replacement_map[value]
-
-
-def translate(sql, column_map):
-    parsed_query = parse(sql)
-
-    # check if it's a url, otherwise raise exception
-    from_ = parsed_query.pop('from')
-
-    for clause in find_value_clauses(parsed_query):
-        replace_value(clause, column_map)
-
-    return format(parsed_query)
 
 
 class TranslationTestSuite(unittest.TestCase):
