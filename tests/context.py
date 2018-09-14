a516afffb0a6cf509a264bce0dd08d74c0075c1c
Beto Dealmeida <roberto@dealmeida.net>
2018-09-14 15:56:02 -0700

    More tests

--- tests/context.py
+++ tests/context.py
@@ -4,4 +4,5 @@ sys.path.insert(0, os.path.abspath(
     os.path.join(os.path.dirname(__file__), '..')))
 
 import gsheetsdb
+from gsheetsdb import exceptions
 from gsheetsdb.translator import translate
--- tests/test_translation.py
+++ tests/test_translation.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 
-from .context import gsheetsdb, translate
+from .context import exceptions, gsheetsdb, translate
 
 import unittest
 
@@ -9,16 +9,71 @@ from moz_sql_parser import parse, format
 
 class TranslationTestSuite(unittest.TestCase):
 
-    def test_remove_from(self):
+    def test_select(self):
+        sql = 'SELECT country FROM "http://example.com"'
+        expected = "SELECT A"
+        result = translate(sql, {'country': 'A'})
+        self.assertEquals(result, expected)
+
+    def test_from(self):
         sql = 'SELECT * FROM table'
         expected = 'SELECT *'
         result = translate(sql, {})
         self.assertEquals(result, expected)
 
-    def test_column_rename(self):
+    def test_where(self):
+        sql = 'SELECT country FROM "http://example.com" WHERE cnt > 10'
+        expected = 'SELECT A WHERE B > 10'
+        result = translate(sql, {'country': 'A', 'cnt': 'B'})
+        self.assertEquals(result, expected)
+
+    def test_groupby(self):
         sql = 'SELECT country, SUM(cnt) FROM "http://example.com" GROUP BY country'
         expected = "SELECT A, SUM(B) GROUP BY A"
         result = translate(sql, {'country': 'A', 'cnt': 'B'})
+        self.assertEquals(result, expected)
+
+    def test_having(self):
+        sql = '''
+    SELECT
+        country
+      , SUM(cnt)
+    FROM
+        "http://example.com"
+    GROUP BY
+        country
+    HAVING
+        COUNT(*) > 0
+        '''
+        with self.assertRaises(exceptions.NotSupportedError):
+            result = translate(sql, {'country': 'A', 'cnt': 'B'})
+
+    def test_orderby(self):
+        sql = '''
+    SELECT
+        country
+      , SUM(cnt)
+    FROM
+        "http://example.com"
+    GROUP BY
+        country
+    ORDER BY
+        SUM(cnt)
+        '''
+        expected = "SELECT A, SUM(B) GROUP BY A ORDER BY SUM(B)"
+        result = translate(sql, {'country': 'A', 'cnt': 'B'})
+        self.assertEquals(result, expected)
+
+    def test_limit(self):
+        sql = 'SELECT country FROM "http://example.com" LIMIT 10'
+        expected = 'SELECT A LIMIT 10'
+        result = translate(sql, {'country': 'A'})
+        self.assertEquals(result, expected)
+
+    def test_where(self):
+        sql = 'SELECT country FROM "http://example.com" LIMIT 10 OFFSET 5'
+        expected = 'SELECT A LIMIT 10 OFFSET 5'
+        result = translate(sql, {'country': 'A'})
         self.assertEquals(result, expected)
 
 
