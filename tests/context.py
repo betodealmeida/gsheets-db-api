fc07f90a33fe4fbbdd2c42984c6f06f11276822e
Beto Dealmeida <roberto@dealmeida.net>
2018-09-21 12:46:47 -0700

    Adding unit tests

--- tests/context.py
+++ tests/context.py
@@ -8,6 +8,12 @@ import gsheetsdb
 from gsheetsdb import exceptions
 from gsheetsdb.convert import convert_rows
 from gsheetsdb.processors import CountStar, is_subset, Processor, SubsetMatcher
+from gsheetsdb.query import (
+    execute,
+    get_column_map,
+    get_description_from_payload,
+    run_query,
+)
 from gsheetsdb.translator import extract_column_aliases, translate
 from gsheetsdb.utils import format_gsheet_error, format_moz_error
 from gsheetsdb.url import extract_url, get_url
