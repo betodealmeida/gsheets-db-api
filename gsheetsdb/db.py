0ab548e05055a36aeae72ad25a23654b8e71912b
Beto Dealmeida <roberto@dealmeida.net>
2018-09-21 21:34:09 -0700

    Remove unused import

--- gsheetsdb/db.py
+++ gsheetsdb/db.py
@@ -3,8 +3,6 @@ from __future__ import division
 from __future__ import print_function
 from __future__ import unicode_literals
 
-import itertools
-
 from six import string_types
 
 from gsheetsdb.exceptions import Error, NotSupportedError
