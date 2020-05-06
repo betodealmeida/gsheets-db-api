f262599fa66cb75f59dfc7746d96dd76657508ac
Beto Dealmeida <roberto@dealmeida.net>
2020-05-06 14:38:54 -0700

    Remove 2.7

--- setup.py
+++ setup.py
@@ -137,7 +137,6 @@ setup(
         # Full list: https://pypi.python.org/pypi?%3Aaction=list_classifiers
         'License :: OSI Approved :: MIT License',
         'Programming Language :: Python',
-        'Programming Language :: Python :: 2.7',
         'Programming Language :: Python :: 3.4',
         'Programming Language :: Python :: 3.5',
         'Programming Language :: Python :: 3.6',
